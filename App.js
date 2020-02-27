cc68f971acfd00104f2f4a1c7b17e7a5aefb88b0
arkhurst <fiiarkhurst@gmail.com>
2020-02-27 19:21:26 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {View, Text, Button, ScrollView, Image} from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+import HomeScreen from './components/homeScreen';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+const Stack = createStackNavigator();
+
+const LogoTitle = () => {
+  return(
+    <View>
+      <View style={{alignItems:'center', flexDirection:'row', justifyContent:'center'}}>
+        <Text style={{fontSize:25, color:'white', fontFamily:'Palatino-Italic'}}>Products</Text>
+      </View>
+      <View style={{ width:16, height:16,  backgroundColor:'red', borderRadius:15, justifyContent:'center', alignItems:'center' , marginLeft:380, flexDirection:'row'}}>
+        <Text style={{fontSize:10, color:'white'}}>2</Text>
+      </View>
+    </View>
+    
+  );
+};
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+         <Stack.Screen 
+         name="Home" 
+         component={HomeScreen}
+         options={{
+           headerStyle: {
+            backgroundColor: 'whitesmoke',
+          },
+          headerTitle: () => < LogoTitle />,
+          headerLeft: () => (
+              <TouchableOpacity style={{marginLeft:15, marginBottom:5}} >
+                <Ionicons 
+                name="ios-menu"
+                size={26}
+                color="white"
+              />
+              </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity style={{ marginRight:20, marginBottom:5}}>
+               <FontAwesome 
+               name="cart-plus"
+               size={26}
+               color="white" />    
+           </TouchableOpacity>
+          ), 
+          headerStyle: {
+            backgroundColor:"#528AD4"
+          }
+          }}
+         />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ components/homeScreen.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import {View, Text, Button, ScrollView, Image} from 'react-native';
+
+const ListItem = [
+  {
+    "id": 1,
+    "product": "iPhone 6s",
+    "price" : 950,
+    "image" : require('../assets/phone.jpg'),
+     inCart:false
+  },
+  {
+    "id": 2,
+    "product": "Galaxy S7",
+    "price" : 650,
+    "image" : require('../assets/phone.jpg'),
+     inCart:false
+  },
+  {
+    "id": 3,
+    "product": "Huawei Mate",
+    "price" : 720,
+    "image" : require('../assets/phone.jpg'),
+     inCart:false
+  },
+  {
+    "id": 4,
+    "product": "Razr",
+    "price" : 500,
+    "image" : require('../assets/phone.jpg'),
+     inCart:false
+  },
+  {
+    "id": 5,
+    "product": "Oppo Reno",
+    "price" : 690,
+    "image" : require('../assets/phone.jpg'),
+     inCart:false
+  },
+]
+
+const ShoppingCart  = () => {
+
+  const [products, setProducts] = useState(ListItem);
+  const [cart, updateCart] = useState([]);
+
+
+  const addProducts = (product) => {
+    const newProduct = {
+      id : product.id,
+      product : product.product,
+      price : product.price
+    }
+    const updatedCart = [newProduct, ...cart]
+    updateCart(updatedCart)
+  }
+  return(
+    <ScrollView style={{flexDirection:'', paddingLeft:20,  paddingRight:20, backgroundColor:'white'}}>
+      <View>
+      <Text style={{fontSize:20, textAlign:"auto", marginTop:10}}>Categories</Text>
+           <ScrollView horizontal={true} style={{marginTop:25, flexDirection:'row'}}>
+             <View style={{backgroundColor:'whitesmoke',height:50, width:120, justifyContent:'center', borderRadius:10, borderColor:"#528AD4", borderWidth:1}}>
+                <Button color="black" title="Phones" />
+             </View>
+             <View style={{backgroundColor:'whitesmoke', marginLeft: 15,height:50, width:120, justifyContent:'center', borderRadius:10, borderColor:"#528AD4", borderWidth:1}}>
+                <Button color="black" title="Laptops" />
+             </View>
+             <View style={{backgroundColor:'whitesmoke', marginLeft: 15,height:50, width:120, justifyContent:'center', borderRadius:10, borderColor:"#528AD4", borderWidth:1}}>
+                <Button color="black" title="Chargers" />
+             </View>
+             <View style={{backgroundColor:'whitesmoke', marginLeft: 15,height:50, width:120, justifyContent:'center', borderRadius:10, borderColor:"#528AD4", borderWidth:1}}>
+                <Button color="black" title="Earphones" />
+             </View>
+           </ScrollView>
+      </View>
+      <Text style={{fontSize:20,marginTop:50}}>Shopping List</Text>
+      {products.map(product => {
+        return(
+          <View key={product.id} >
+            <View style={{ alignItems:'center', backgroundColor:'whitesmoke', justifyContent:'center', marginTop:20, paddingTop:30, borderRadius:8}}>
+               <Image source={product.image} style={{width:250, height:150}} />
+               <View style={{alignItems:'center', marginTop:10, flexDirection:'row', justifyContent:'space-between', paddingRight:20}}>
+                 <Text style={{fontSize:15, fontWeight:'bold'}}> {product.product}</Text>
+                 <View style={{paddingLeft:100}}>
+                   <Text> GHS {product.price}</Text>
+                 </View>
+               </View>
+               <View style={{flexDirection:'row'}}>
+                  <View style={{marginBottom:20, marginTop:20, borderRadius:5, backgroundColor:'#528AD4'}}>
+                    <Button color="white"   title="add to cart" onPress = {() => addProducts(product)} />
+                  </View> 
+                  <View style={{marginBottom:20, marginLeft:20, marginTop:20, borderRadius:5, backgroundColor:'#F78FCC'}}>
+                    <Button color="white"   title="buy now" onPress = {() => addProducts(product)} />
+                 </View> 
+               </View>
+              
+            </View>
+          </View>
+        )
+      })} 
+    </ScrollView>
+  );
+};
+
+export default ShoppingCart;
