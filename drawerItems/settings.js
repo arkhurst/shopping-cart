c88ad28c11b976c828ee8c3719ee28a96115f8aa
unknown <fiiarkhurst@gmail.com>
2020-03-24 16:13:02 +0000

    modified settings screen

--- drawerItems/settings.js
+++ drawerItems/settings.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 const Settings = () => {
     return(
         <View style={styles.container}>
             <Text style={{fontWeight:'500', marginTop:30,paddingHorizontal:15}}>About DEJAYS</Text>
             <View style={styles.itemContainer}>
-                <Text style={{fontWeight:'bold', color:'grey'}}>Our Services</Text>
-                <Text style={{fontWeight:'bold', color:'grey'}}>Terms and Conditions</Text>
-                <Text style={{fontWeight:'bold', color:'grey'}}>FAQ</Text>
+                <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>Our Services</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>Terms and Conditions</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>FAQ</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
             </View>
             <Text style={{fontWeight:'500', marginTop:10,paddingHorizontal:15}}>Settings</Text>
             <View style={styles.itemContainer}>
-               <Text style={{fontWeight:'bold', color:'grey'}}>Push Notifications</Text>
-                <Text style={{fontWeight:'bold', color:'grey'}}>Country</Text>
-                <Text style={{fontWeight:'bold', color:'grey'}}>Language</Text>
+                 <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>Push Notifications</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>Country</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback>
+                    <View style={styles.items}>
+                      <Text style={{fontWeight:'bold', color:'grey'}}>Language</Text>
+                      <Ionicons name="ios-arrow-forward" size={25}  color='grey' />
+                    </View>
+                </TouchableWithoutFeedback>
+              
             </View>
         </View>
     );
@@ -32,6 +65,10 @@ const styles = StyleSheet.create({
         width:415,
         paddingHorizontal:15,
         marginVertical:10
+    },
+    items:{
+        flexDirection:'row',
+        justifyContent:"space-between"
     }
 })
 
